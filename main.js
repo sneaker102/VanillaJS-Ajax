192a7264841625aefd61f1ff71ff945a8cb5e8a8
sneaker102 <sneaker102@yahoo.com>
2020-04-16 15:08:33 -0700

    dynamic notes creation and events

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const bodyContentHtml = document.getElementById('body-content');
 const noteTextHtml = document.getElementById('note-text');
 const cardMainContentHtml = document.getElementById('card-main-content');
 const createNoteArticleHtml = document.getElementById('create-note-article');
-const userImgContainerHtml = document.getElementById('user-img-container');
 const secondUploadImgHtml = document.getElementById('second-upload-img');
 const chooseColorHtml = document.getElementById('choose-color');
 
@@ -17,15 +16,15 @@ const viewNotes = [];
 //api URl
 const apiUrl = 'http://localhost:3000/notes';
 // on init
-(function() {
+(function () {
     fetch(apiUrl, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         }
     }).then(resp => resp.json())
-    .then(data => createViewNotes(data));
- })();
+        .then(data => createViewNotes(data));
+})();
 
 // search event
 const search$ = Rx.Observable.fromEvent(searchInputHtml, 'keyup')
@@ -48,38 +47,96 @@ createNoteArticleHtml.addEventListener('click', buildFullNoteCard);
 
 //restore initial note card or submit
 const submitEvents$ = Rx.Observable
-.merge(...[
-    Rx.Observable.fromEvent(pinNoteHtml, 'click'),
-     Rx.Observable.fromEvent(bodyContentHtml, 'click')])
-     .throttleTime(300).subscribe(e => submitNote(e));
+    .merge(...[
+        Rx.Observable.fromEvent(pinNoteHtml, 'click'),
+        Rx.Observable.fromEvent(bodyContentHtml, 'click')])
+    .throttleTime(300).subscribe(e => submitNote(e));
 //upload imgs events
-createNoteWithImgHtml.addEventListener('click', openFileDialog);
-secondUploadImgHtml.addEventListener('click', openFileDialog);
+createNoteWithImgHtml.addEventListener('click', newNoteUploadFile);
+secondUploadImgHtml.addEventListener('click', newNoteUploadFile);
 
 //choose color event
-chooseColorHtml.addEventListener('mouseenter', chooseColor);
+chooseColorHtml.addEventListener('mouseenter', chooseColor.bind(this, chooseColorHtml, createNoteArticleHtml));
 
 //textarea resize event
 noteTextHtml.addEventListener('input', resizeTextarea);
 
-//on image container hover
-userImgContainerHtml.addEventListener('mouseenter', () =>
-    document.getElementById('remove-img').style.cssText = 'display:inline-block !important');
-userImgContainerHtml.addEventListener('mouseleave', () =>
-    document.getElementById('remove-img').style.cssText = 'display:none !important');
 
 //clean up onDestroy
 window.addEventListener('beforeunload', clearEvents);
 
 function createViewNotes(data) {
-    if(Array.isArray(data)) {
-        viewNotes.push(...data);
-        // viewNotes.forEach(note => {
-        // const cardLayout =  document.createElemen('article');
-        // cardLayout.className('create-note created-note');
-
-        // note.cardLayoutHtml = cardLayout;
-        // });
+    if (Array.isArray(data)) {
+        data.forEach(note => {
+            const cardLayout = document.createElement('article');
+            cardLayout.className = 'create-note created-note';
+            cardLayout.style.backgroundColor = note.color;
+            const uploadedImgContainer = document.createElement('aside');
+            uploadedImgContainer.className = 'user-img-container';
+            if (note.img.length) {
+                const imgHtml = document.createElement('img');
+                imgHtml.src = note.img;
+                imgHtml.style.maxHeight = '36.4rem';
+                imgHtml.style.maxWidth = '36.4rem';
+                //img container
+                uploadedImgContainer.style.display = 'flex';
+                const dummyHtml = document.createElement('div');
+                dummyHtml.innerHTML = '<i class="far fa-trash-alt remove-img"></i>'
+                uploadedImgContainer.appendChild(imgHtml);
+                uploadedImgContainer.appendChild(dummyHtml.firstChild);
+                //on remove img 
+                uploadedImgContainer.childNodes[1].addEventListener('click', () => uploadedImgContainer.parentNode.removeChild(uploadedImgContainer));
+                //on image container hover
+                uploadedImgContainer.addEventListener('mouseenter', () =>
+                    uploadedImgContainer.childNodes[1].style.cssText = 'display:inline-block !important');
+                uploadedImgContainer.addEventListener('mouseleave', () =>
+                    uploadedImgContainer.childNodes[1].style.cssText = 'display:none !important');
+                cardLayout.appendChild(uploadedImgContainer);
+            }
+            const headerSection = document.createElement('section');
+            const inputTitle = document.createElement('input');
+            inputTitle.type = 'text';
+            inputTitle.placeholder = 'Title';
+            inputTitle.className = 'u-transparent';
+            inputTitle.value = note.title;
+            headerSection.appendChild(inputTitle);
+            const pinImg = document.createElement('i');
+            pinImg.className = 'fas fa-thumbtack pin-note';
+            headerSection.appendChild(pinImg);
+            cardLayout.appendChild(headerSection);
+            const mainContentSection = document.createElement('section');
+            const textareaDescription = document.createElement('textarea');
+            textareaDescription.className = 'u-transparent';
+            textareaDescription.placeholder = 'Create a note...';
+            textareaDescription.value = note.description;
+            mainContentSection.appendChild(textareaDescription);
+            const mainContentFooter = document.createElement('footer');
+            const spanContainer = document.createElement('span');
+            const brushIcon = document.createElement('i');
+            brushIcon.className = 'fas fa-paint-brush';
+            brushIcon.addEventListener('mouseenter', chooseColor.bind(this, brushIcon, cardLayout, event));
+            spanContainer.appendChild(brushIcon);
+            const uploadImgIcon = document.createElement('i');
+            uploadImgIcon.className = 'far fa-image';
+            uploadImgIcon.addEventListener('click', openFileDialog.bind(this, uploadedImgContainer, false))
+            spanContainer.appendChild(uploadImgIcon);
+            mainContentFooter.appendChild(spanContainer);
+            const closeBtn = document.createElement('button');
+            closeBtn.className = 'u-transparent';
+            closeBtn.innerText = 'Close';
+            closeBtn.style.display = 'none';
+            mainContentFooter.appendChild(closeBtn);
+            mainContentSection.appendChild(mainContentFooter);
+            cardLayout.appendChild(mainContentSection);
+            if (note.img.length) {
+                // const modal = document.createElement('div');
+                // modal.className = 'modal';
+                // modal.appendChild(cardLayout);
+                // document.getElementById('created-note-container').appendChild(modal);
+            } else {
+            }
+            document.getElementById('created-note-container').appendChild(cardLayout);
+        });
     }
 }
 function submitNote(e) {
@@ -104,21 +161,21 @@ function submitNote(e) {
                 color: noteColor,
                 img: noteImg
             };
-                fetch(apiUrl, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(myNote)
-                })
-                 .then(_ => buildCreateNoteCard(e));
+            fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(myNote)
+            })
+                .then(_ => buildCreateNoteCard(e));
         } else {
             buildCreateNoteCard(e);
         }
     }
 }
-function chooseColor() {
-    if (!chooseColorHtml.childNodes.length) {
+function chooseColor(btn, containerToColor) {
+    if (!btn.childNodes.length) {
         // create colors container
         const colorContainerHtml = document.createElement('div');
         colorContainerHtml.style.display = 'flex';
@@ -141,7 +198,7 @@ function chooseColor() {
             const colorSpanHtml = document.createElement('span');
             colorSpanHtml.style.backgroundColor = color;
             let className = 'circle';
-            if (getStyle(createNoteArticleHtml, 'backgroundColor') === color) {
+            if (getStyle(containerToColor, 'backgroundColor') === color) {
                 className += ' selected';
                 colorSpanHtml.appendChild(checkMark);
             }
@@ -155,10 +212,10 @@ function chooseColor() {
                 colorContainerHtml.childNodes[colorContainerHtml.childNodes.length - 1].appendChild(colorSpanHtml);
             }
             allColorSpan.push(colorSpanHtml);
-            colorSpanHtml.addEventListener('click', selectColor.bind(null, createNoteArticleHtml, colorSpanHtml, allColorSpan, event));
+            colorSpanHtml.addEventListener('click', selectColor.bind(this, containerToColor, colorSpanHtml, allColorSpan));
         });
-        chooseColorHtml.appendChild(colorContainerHtml);
-        colorContainerHtml.addEventListener('mouseleave', () => chooseColorHtml.removeChild(colorContainerHtml));
+        btn.appendChild(colorContainerHtml);
+        colorContainerHtml.addEventListener('mouseleave', () => btn.removeChild(colorContainerHtml));
     }
 }
 function selectColor(elToColor, elToMark, allElColors, e) {
@@ -175,31 +232,29 @@ function selectColor(elToColor, elToMark, allElColors, e) {
         }
     }
 }
-function removeImg() {
-    const userImgHtml = userImgContainerHtml.childNodes[userImgContainerHtml.childNodes.length - 2];
-    if (!!userImgHtml && userImgHtml.tagName === 'IMG') {
-        userImgContainerHtml.removeChild(userImgHtml);
-        userImgContainerHtml.style.display = 'none';
-        createNoteInputHtml.focus();
-    }
+function newNoteUploadFile(e) {
+    const imgContainer = document.createElement('aside');
+    imgContainer.className = 'user-img-container';
+    createNoteArticleHtml.insertBefore(imgContainer, document.getElementById('card-header-section'));
+    openFileDialog(imgContainer, true, e);
 }
-function openFileDialog(e) {
+function openFileDialog(imgContainer, isNewNote, e) {
     e.stopPropagation();
     const fileInputHtml = document.createElement('input');
     fileInputHtml.type = 'file';
     fileInputHtml.accept = 'image/*';
     fileInputHtml.multiple = false;
-    fileInputHtml.addEventListener('change', fileDialogChanged);
+    fileInputHtml.addEventListener('change', fileDialogChanged.bind(this, imgContainer, isNewNote));
     fileInputHtml.dispatchEvent(new MouseEvent('click'));
 }
 
-function fileDialogChanged(e) {
+function fileDialogChanged(imgContainer, isNewNote, e) {
     if (e.target.files && e.target.files[0]) {
         const fileName = e.target.files[0].name;
         const reader = new FileReader();
         reader.onload = function (fileLoadEvent) {
-            const previousImgHtml = document.getElementById('user-img-upload');
-            if (!!!previousImgHtml) {
+            // second child is the actual img
+            if (!imgContainer.childNodes.length) {
                 const imgHtml = document.createElement('img');
                 imgHtml.id = 'user-img-upload';
                 imgHtml.src = fileLoadEvent.target.result;
@@ -208,18 +263,23 @@ function fileDialogChanged(e) {
                 imgHtml.style.maxHeight = '45rem';
                 imgHtml.style.maxWidth = '45rem';
                 //img container
-                userImgContainerHtml.style.display = 'flex';
+                imgContainer.style.display = 'flex';
                 const dummyHtml = document.createElement('div');
-                dummyHtml.innerHTML = '<i id="remove-img" class="far fa-trash-alt remove-img"></i>'
-                userImgContainerHtml.appendChild(imgHtml);
-                userImgContainerHtml.appendChild(dummyHtml.firstChild);
+                dummyHtml.innerHTML = '<i class="far fa-trash-alt remove-img"></i>'
+                imgContainer.appendChild(imgHtml);
+                imgContainer.appendChild(dummyHtml.firstChild);
                 //on remove img 
-                userImgContainerHtml.childNodes[userImgContainerHtml.childNodes.length - 1].addEventListener('click', removeImg);
+                imgContainer.childNodes[1].addEventListener('click', () => imgContainer.parentNode.removeChild(imgContainer));
+                //on image container hover
+                imgContainer.addEventListener('mouseenter', () =>
+                    imgContainer.childNodes[1].style.cssText = 'display:inline-block !important');
+                imgContainer.addEventListener('mouseleave', () =>
+                    imgContainer.childNodes[1].style.cssText = 'display:none !important');
             } else {
-                previousImgHtml.src = fileLoadEvent.target.result;
+                imgContainer.childNodes[0].src = fileLoadEvent.target.result;
             }
-            if (cardMainContentHtml.style.display === ''
-                || cardMainContentHtml.style.display === 'none') {
+            if (!!isNewNote && (cardMainContentHtml.style.display === ''
+                || cardMainContentHtml.style.display === 'none')) {
                 buildFullNoteCard(e)
             }
         };
@@ -231,7 +291,7 @@ function buildCreateNoteCard(e) {
     if (e.srcElement.id === 'body-content' || e.srcElement.id === 'pin-note'
         || e.srcElement.id === 'close-note') {
         e.stopPropagation();
-        removeImg();
+        // removeImg();
         noteTextHtml.value = '';
         cardMainContentHtml.style.display = 'none';
         createNoteInputHtml.value = '';
